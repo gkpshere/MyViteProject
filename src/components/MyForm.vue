0a26def398884155b84d9ca8f2d4aa8b74777521
Gayathri <gkpshere@gmail.com>
2024-02-28 15:58:00 +0530

    first commit

---
+++ src/components/MyForm.vue
@@ -0,0 +1,58 @@
+<template>
+<div>
+
+    <div>
+    <input ref="el" type="text" placeholder="Name" v-model="name"/>
+    </div>
+    <div>
+    <button @click="submitForm" 
+    v-bind:disabled="name.length==0"
+    >Submit
+</button>
+    </div>
+    </div>
+</template>
+
+<script>
+import {ref, onMounted,onUpdated} from "vue";
+
+export default {
+    
+    setup() {
+        console.log("The component is now set up")
+        const name= ref("");
+        const el = ref()
+        onMounted(()=>{
+        console.log("the component is rendered once and we are in the on mounted lifecycle")
+        el.value.focus();
+    })
+
+    onUpdated(()=>{
+        console.log("Updated")
+        
+    })
+        
+        const submitForm= ()=>{console.log(`form submitted, name=${name.value}`)}
+        return{
+            el,
+            name,
+            submitForm
+        }
+        
+    }
+};
+</script>
+
+<style>
+input[type="text"]{
+    padding:10px;
+    border-style:dotted;
+    margin:10px;
+}
+button{
+    margin-top:10px;
+   
+    
+
+}
+</style>
